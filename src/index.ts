6116608c0f80af3290f4b3ddcae86a063d0681a9
Nino <166747012+ninoc3@users.noreply.github.com>
2025-06-12 22:56:25 +0200

    Delete src/index.ts

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { API } from 'homebridge';
-import { ReolinkMotionAccessory } from './src/accessory';
-
-export = (api: API) => {
-  api.registerAccessory('HomebridgeReolinkMotion', ReolinkMotionAccessory);
-};
